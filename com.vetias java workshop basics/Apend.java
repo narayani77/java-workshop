9aaf37e56901286f8ca98d3bfc5b3de488f7f71d
narayani77 <narayinih24it@vetias.ac.in>
2025-07-07 14:42:40 +0530

    Apend.java

---
+++ com.vetias java workshop basics/Apend.java
@@ -0,0 +1,16 @@
+public class Apend{
+
+   public static void main(String[] args) {
+    StringBuilder sentence=new StringBuilder();
+    sentence.append(" java");
+    sentence.append(" is");
+    sentence.append(" the");
+    sentence.append(" most");
+   sentence.append(" used");
+    sentence.append(" language");
+    sentence.append(" for");
+    sentence.append(" enterprises");
+    sentence.append(" application");
+    System.out.println(sentence.toString());
+   }
+}
--- com.vetias java workshop basics/word.java
+++ com.vetias java workshop basics/word.java
@@ -1,12 +1,12 @@
 public class word {
 
     public static void main(String[] args){
-        String sentence ="this program shows how to splite a string we need to find the sentence";
-        String[] sentences =text.split(regex="\\");
-        int sentence count=text.length;
-        String[] words =text .split(regrx);
+        String sentence ="this program shows how to splite. a string we need to find the sentence";
+        String[] sentences =sentence.split("\\.");
+        int  count=sentences.length;
+        String[] words =sentences .split(regex    :" ")
         int wordcount = word.length;
-        String[] letter = text.split(regrex=" ")
+        String[] letter = sentence.split(=" ");
         int lettercount = letter.length ;
         System.out.println("no.of letters:"+letters);
     }
